7e42ecde5b4303a3d19ce52b0e99dd126c45948e
Zakum <mirveo@gmail.com>
2023-12-07 14:34:35 -0800

    finish DB & simplify query

--- create_db.js
+++ create_db.js
@@ -1,6 +1,7 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 const fs = require("fs");
+const uniq = require("lodash/uniq")
 
 // Path to your JSON file
 const jsonFilePath = "./jmdict-eng-common-3.5.0.json";
@@ -15,134 +16,45 @@ const db = new sqlite3.Database(dbPath);
 db.serialize(() => {
     db.run(`
         CREATE TABLE IF NOT EXISTS Words (
-        id TEXT PRIMARY KEY
+        id TEXT PRIMARY KEY,
+        entry_data TEXT,
+        kanjis TEXT,
+        kanas TEXT,
+        meanings TEXT,
+        tags TEXT
         )
     `);
-
-    // Create tables
-    db.run(`CREATE TABLE IF NOT EXISTS Kanji (
-        id TEXT PRIMARY KEY,
-        entry_id TEXT,
-        common INTEGER,
-        text TEXT,
-        FOREIGN KEY (entry_id) REFERENCES Words(id)
-    )`);
-
-    db.run(`CREATE TABLE IF NOT EXISTS Kana (
-        id TEXT PRIMARY KEY,
-        entry_id TEXT,
-        common INTEGER,
-        text TEXT,
-        FOREIGN KEY (entry_id) REFERENCES Words(id)
-    )`);
-
-    db.run(`CREATE TABLE IF NOT EXISTS Sense (
-        id TEXT PRIMARY KEY,
-        entry_id TEXT,
-        gloss TEXT,
-        partOfSpeech TEXT,
-        appliesToKanji TEXT,
-        appliesToKana TEXT,
-        related TEXT,
-        antonym TEXT,
-        field TEXT,
-        dialect TEXT,
-        misc TEXT,
-        info TEXT,
-        languageSource TEXT,
-        FOREIGN KEY (entry_id) REFERENCES Words(id)
-    )`);
-
+    console.log("Created table Words")
 
     // Insert data into the table
     jsonData.forEach((entry) => {
-        db.run(`INSERT INTO Words (id) VALUES (?)`, [entry.id], (err) => {
+        const { id } = entry
+        const entry_data = JSON.stringify(entry)
+        const kanjis = entry.kanji.map(kanji => kanji.text).join("$ ")
+        const kanas = entry.kana.map(kana => kana.text).join("$ ")
+        const meanings = entry.sense.map(sense =>
+            sense.gloss.map(gloss => gloss.text).join("$ ")
+        ).join("")
+
+        const allTags = entry.sense.flatMap(sense => 
+            [...sense.field, ...sense.dialect, ...sense.misc])
+
+        if ([...entry.kanji, ...entry.kana].some(obj => obj.common)) {
+            allTags.push("common")
+        }
+
+        const uniqueTags = uniq(tags).join("$ ")
+
+        db.run(`INSERT INTO Words (id, entry_data, kanjis, kanas, meanings, tags) VALUES (?, ?, ?, ?, ?, ?)`, [id, entry_data, kanjis, kanas, meanings, uniqueTags], (err) => {
             if (err) {
                 console.error(
-                    `Error inserting word ${entry.id} into table Words:`,
+                    `Error inserting word ${id} into table Words:`,
                     err.message
                 );
             } else {
-                console.log(`Inserted word ${entry.id} into Words`);
+                console.log(`Inserted word ${id} into Words`);
             }
         });
-
-        // Inset kanji into Kanji table
-        entry.kanji.forEach((kanji, kanjiIndex) => {
-            const kanji_id = `${entry.id}_kanji_${kanjiIndex}`;
-
-            db.run(
-                `INSERT INTO Kanji (id, entry_id, common, text) VALUES (?, ?, ?, ?)`,
-                [kanji_id, entry.id, kanji.common, kanji.text],
-                (err) => {
-                    if (err) {
-                        console.error(
-                            `Error inserting kanji ${kanji_id} (word ${entry.id}) into table Kanji:`,
-                            err.message
-                        );
-                    } else {
-                        console.log(
-                            `Inserted kanji ${kanji_id} (word ${entry.id}) into table Kanji`
-                        );
-                    }
-                }
-            );
-        });
-
-        // Inset kana into Kana table
-        entry.kana.forEach((kana, kanaIndex) => {
-            const kana_id = `${entry.id}_kana_${kanaIndex}`;
-
-            db.run(
-                `INSERT INTO Kana (id, entry_id, common, text) VALUES (?, ?, ?, ?)`,
-                [kana_id, entry.id, kana.common, kana.text],
-                (err) => {
-                    if (err) {
-                        console.error(
-                            `Error inserting kana ${kana_id} (word ${entry.id}) into table Kana:`,
-                            err.message
-                        );
-                    } else {
-                        console.log(
-                            `Inserted kana ${kana_id} (word ${entry.id}) into table Kana`
-                        );
-                    }
-                }
-            );
-        });
-
-
-        entry.sense.forEach((sense, senseIndex) => {
-            const sense_id = `${entry.id}_sense_${senseIndex}`;
-            const gloss = sense.gloss.map(obj => obj.text).join("$ ")
-            const partOfSpeech = sense.partOfSpeech.join("$ ")
-            const appliesToKanji = sense.appliesToKanji.join("$ ")
-            const appliesToKana = sense.appliesToKana.join("$ ")
-            const related = sense.related.join("$ ")
-            const antonym = sense.antonym.join("$ ")
-            const field = sense.field.join("$ ")
-            const dialect = sense.dialect.join("$ ")
-            const misc = sense.misc.join("$ ")
-            const info = sense.info.join("$ ")
-            const languageSource = sense.languageSource.join("$ ")
-
-            db.run(
-                `INSERT INTO Sense (id, entry_id, gloss, partOfSpeech, appliesToKanji, appliesToKana, related, antonym, field, dialect, misc, info, languageSource) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-                [sense_id, entry.id, gloss, partOfSpeech, appliesToKanji, appliesToKana, related, antonym, field, dialect, misc, info, languageSource],
-                (err) => {
-                    if (err) {
-                        console.error(
-                            `Error inserting sense ${sense_id} (word ${entry.id}) into table Sense:`,
-                            err.message
-                        );
-                    } else {
-                        console.log(
-                            `Inserted kana ${sense_id} (word ${entry.id}) into table Sense`
-                        );
-                    }
-                }
-            );
-        })
     });
 });
 
--- query.js
+++ query.js
@@ -5,51 +5,52 @@ const path = require("path");
 const dbPath = path.join(__dirname, "your-database.db");
 const db = new sqlite3.Database(dbPath);
 
-// Specify the ID you want to query
-// const targetId = "求める";
+const query = 
+`SELECT id, kanjis, kanas, meanings, tags 
+FROM Words 
+WHERE kanjis 
+LIKE ? 
+OR kanas 
+LIKE ? 
+OR kanas
+LIKE ? 
+OR meanings 
+LIKE ?`;
 
-// Query to retrieve the row with the specified ID from the "entries" table
-// const query = "SELECT * FROM entries WHERE id = ?";
+// data for search:
+// CDプレーヤー, CDプレイヤー  [シーディープレーヤー, シーディープレイヤー]
+// CD Player
+// tags
 
-// Query to retrieve rows where the "fruit" column contains the specified substring
-const query = 'SELECT * FROM Sense WHERE gloss LIKE ?';
+// case insensitive
+const meaningQueryString = "cd player";
+const hiraganaReadingQueryString = "しくじる"
+const katakanaReadingQueryString = "アイヌ"
+const kanjiWordQueryString = "其れ" 
 
-const searchString = "after";
 
-db.all(query, [`%${searchString}%`], (err, rows) => {
-  if (err) {
-    console.error("Error executing query:", err.message);
-  } else {
-    if (rows.length > 0) {
-      console.log("Results:", rows);
+// kanjis kanas meanings
+db.all(query, [`%${kanjiWordQueryString}%`, `%${hiraganaReadingQueryString}%`, `%${katakanaReadingQueryString}%`, `%${meaningQueryString}%`], (err, rows) => {
+    if (err) {
+        console.error("Error executing query:", err.message);
     } else {
-      console.log(
-        `No entry found containing the substring "${searchString}" in the 'text' column`
-      );
+        if (rows.length > 0) {
+            console.log("Results:", rows, `Number of results: ${rows.length}`);
+        } else {
+            console.log(
+                `No entry found`
+            );
+        }
     }
-  }
-// });
 
-// Execute the query with the specified ID and handle the result
-// db.get(query, [targetId], (err, row) => {
-//   if (err) {
-//     console.error("Error executing query:", err.message);
-//   } else {
-//     if (row) {
-//       console.log("Result:", row);
-//     } else {
-//       console.log(`No entry found with ID ${targetId}`);
-//     }
-//   }
-
-  // Close the database connection
-  db.close((closeErr) => {
-    if (closeErr) {
-      return console.error(
-        "Error closing database connection:",
-        closeErr.message
-      );
-    }
-    console.log("Database connection closed.");
-  });
+    // Close the database connection
+    db.close((closeErr) => {
+        if (closeErr) {
+            return console.error(
+                "Error closing database connection:",
+                closeErr.message
+            );
+        }
+        console.log("Database connection closed.");
+    });
 });
